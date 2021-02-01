33a8fc3546c4e2835da871dc217699dca3ea3cce
agilasoft <dev@agilasoft.com>
2021-02-01 02:45:41 +0000

    modified:   logistics/transport/doctype/transport_booking/transport_booking.js 	modified:   logistics/transport/doctype/transport_booking/transport_booking.json 	modified:   logistics/transport/doctype/transport_booking/transport_booking.py 	modified:   logistics/transport/doctype/transport_booking_instruction/transport_booking_instruction.json 	modified:   logistics/transport/doctype/transport_job/transport_job.json

--- logistics/transport/doctype/transport_booking/transport_booking.js
+++ logistics/transport/doctype/transport_booking/transport_booking.js
@@ -2,7 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Transport Booking', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if(frm.doc.docstatus === 1){
+			frm.add_custom_button(__('Transport Job'), function(){
+				cur_frm.call('create_job','',function(r){});
+			}, __("Create"));
+		}
+	},
 });
--- logistics/transport/doctype/transport_booking/transport_booking.py
+++ logistics/transport/doctype/transport_booking/transport_booking.py
@@ -3,8 +3,25 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class TransportBooking(Document):
 	pass
+
+	@frappe.whitelist()
+	def create_job(self):
+		doc = frappe.new_doc("Transport Job")
+		doc.customer = self.customer
+		doc.job_date = frappe.utils.nowdate()
+		doc.transport_booking = self.name
+		doc.customer_ref_no = self.customer_ref_no
+		doc.hazardous = self.hazardous
+		doc.refrigeration = self.refrigeration
+		doc.vehicles = self.vehicles
+		doc.instructions = self.instructions
+		doc.packages = self.packages
+		doc.internal_notes = self.internal_notes
+		doc.client_notes = self.client_notes
+		doc.flags.ignore_mandatory = True
+		doc.insert()
