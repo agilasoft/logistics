cfa075bd9c510fb76f0f6dc4a0c0120c2c4938c8
agilasoft <info@agilasoft.com>
2025-10-16 06:16:11 +0000

    deleted:    logistics/warehousing/doctype/warehouse_contract/test_warehouse_contract.py 	modified:   logistics/warehousing/doctype/warehouse_contract/warehouse_contract.js 	modified:   logistics/warehousing/doctype/warehouse_contract/warehouse_contract.py

--- logistics/warehousing/doctype/warehouse_contract/warehouse_contract.js
+++ logistics/warehousing/doctype/warehouse_contract/warehouse_contract.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2025, www.agilasoft.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Warehouse Contract", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Warehouse Contract", {
+	refresh(frm) {
+		// Add Get Rates action to the menu
+		if (frm.doc.sales_quote && !frm.is_new()) {
+			frm.add_custom_button(__("Get Rates"), function() {
+				frm.call({
+					method: "logistics.warehousing.doctype.warehouse_contract.warehouse_contract.get_rates_from_sales_quote",
+					args: {
+						warehouse_contract: frm.doc.name,
+						sales_quote: frm.doc.sales_quote
+					},
+					callback: function(r) {
+						if (r.message) {
+							frm.refresh_field("items");
+							frappe.show_alert({
+								message: __("Rates have been successfully imported from Sales Quote"),
+								indicator: "green"
+							});
+						}
+					}
+				});
+			}, __("Actions"));
+		}
+	},
+});
