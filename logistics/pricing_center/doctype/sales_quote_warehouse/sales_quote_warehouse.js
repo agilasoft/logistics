3b1a52e1d8c433a55911f92cf59e02c62e122ba0
agilasoft <info@agilasoft.com>
2025-11-17 17:15:11 +0000

    modified:   logistics/pricing_center/doctype/sales_quote_warehouse/sales_quote_warehouse.js 	modified:   logistics/pricing_center/doctype/sales_quote_warehouse/sales_quote_warehouse.json 	modified:   logistics/pricing_center/doctype/sales_quote_warehouse/sales_quote_warehouse.py 	modified:   logistics/warehousing/doctype/warehouse_contract_item/warehouse_contract_item.json 	modified:   logistics/warehousing/api_parts/billing_methods.py 	modified:   logistics/warehousing/charges.py 	modified:   logistics/warehousing/doctype/warehouse_job/warehouse_job.py

--- logistics/pricing_center/doctype/sales_quote_warehouse/sales_quote_warehouse.js
+++ logistics/pricing_center/doctype/sales_quote_warehouse/sales_quote_warehouse.js
@@ -19,6 +19,44 @@ frappe.ui.form.on('Sales Quote Warehouse', {
 			// Clear unit_cost if item is cleared
 			frappe.model.set_value(cdt, cdn, 'unit_cost', 0);
 		}
+	},
+	calculation_method: function(frm, cdt, cdn) {
+		// Trigger refresh to show/hide dependent fields
+		frm.refresh_field('items');
+		// Calculate estimated revenue when calculation method changes
+		calculate_estimated_revenue(frm, cdt, cdn);
+	},
+	unit_rate: function(frm, cdt, cdn) {
+		calculate_estimated_revenue(frm, cdt, cdn);
+	},
+	minimum_quantity: function(frm, cdt, cdn) {
+		calculate_estimated_revenue(frm, cdt, cdn);
+	},
+	minimum_charge: function(frm, cdt, cdn) {
+		calculate_estimated_revenue(frm, cdt, cdn);
+	},
+	maximum_charge: function(frm, cdt, cdn) {
+		calculate_estimated_revenue(frm, cdt, cdn);
+	},
+	base_amount: function(frm, cdt, cdn) {
+		calculate_estimated_revenue(frm, cdt, cdn);
 	}
 });
+
+function calculate_estimated_revenue(frm, cdt, cdn) {
+	let row = locals[cdt][cdn];
+	if (row.calculation_method && row.unit_rate) {
+		frappe.call({
+			method: 'logistics.pricing_center.doctype.sales_quote_warehouse.sales_quote_warehouse.calculate_estimated_revenue_for_row',
+			args: {
+				doc: row
+			},
+			callback: function(r) {
+				if (r.message && r.message.estimated_revenue !== undefined) {
+					frappe.model.set_value(cdt, cdn, 'estimated_revenue', r.message.estimated_revenue);
+				}
+			}
+		});
+	}
+}
 
