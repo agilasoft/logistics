28bfc35e66d3bb520922141240aed8ef0e9671fa
agilasoft <info@agilasoft.com>
2025-11-28 07:43:34 +0000

    modified:   logistics/warehousing/doctype/handling_unit/handling_unit.js 	modified:   logistics/warehousing/doctype/handling_unit/handling_unit.py

--- logistics/warehousing/doctype/handling_unit/handling_unit.js
+++ logistics/warehousing/doctype/handling_unit/handling_unit.js
@@ -2,27 +2,60 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Handling Unit", {
+	onload(frm) {
+		_toggle_storage_location_size(frm);
+	},
+	
 	refresh(frm) {
 		_toggle_storage_location_size(frm);
 	},
 	
 	company(frm) {
 		_toggle_storage_location_size(frm);
+	},
+	
+	type(frm) {
+		// Also check when type changes since storage_location_size fetches from type
+		_toggle_storage_location_size(frm);
 	}
 });
 
 function _toggle_storage_location_size(frm) {
 	// Disable storage_location_size field if location overflow is not enabled in warehouse settings
-	if (frm.doc.company) {
-		frappe.db.get_value("Warehouse Settings", frm.doc.company, "enable_location_overflow", (r) => {
-			if (r && r.enable_location_overflow) {
-				frm.set_df_property("storage_location_size", "read_only", 0);
-			} else {
-				frm.set_df_property("storage_location_size", "read_only", 1);
-			}
-		});
-	} else {
+	if (!frm.doc.company) {
 		// If no company is set, disable the field
-		frm.set_df_property("storage_location_size", "read_only", 1);
+		if (frm.fields_dict.storage_location_size) {
+			frm.set_df_property("storage_location_size", "read_only", 1);
+			console.log("Handling Unit: storage_location_size disabled - no company");
+		}
+		return;
 	}
+	
+	// Use server method for more reliable checking
+	frappe.call({
+		method: "logistics.warehousing.doctype.handling_unit.handling_unit.is_location_overflow_enabled",
+		args: {
+			company: frm.doc.company
+		},
+		callback: function(r) {
+			if (frm.fields_dict.storage_location_size) {
+				if (r && r.message) {
+					frm.set_df_property("storage_location_size", "read_only", 0);
+					console.log("Handling Unit: storage_location_size enabled - location overflow is ON for company", frm.doc.company);
+				} else {
+					frm.set_df_property("storage_location_size", "read_only", 1);
+					console.log("Handling Unit: storage_location_size disabled - location overflow is OFF for company", frm.doc.company);
+				}
+			} else {
+				console.log("Handling Unit: storage_location_size field not found in form");
+			}
+		},
+		error: function(r) {
+			// On error, disable the field to be safe
+			if (frm.fields_dict.storage_location_size) {
+				frm.set_df_property("storage_location_size", "read_only", 1);
+				console.log("Handling Unit: storage_location_size disabled - error checking warehouse settings", r);
+			}
+		}
+	});
 }
--- logistics/warehousing/doctype/handling_unit/handling_unit.py
+++ logistics/warehousing/doctype/handling_unit/handling_unit.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2025, www.agilasoft.com and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class HandlingUnit(Document):
 	pass
+
+
+@frappe.whitelist()
+def is_location_overflow_enabled(company):
+	"""Check if location overflow is enabled for a company"""
+	if not company:
+		return False
+	try:
+		settings = frappe.get_doc("Warehouse Settings", company)
+		return bool(getattr(settings, "enable_location_overflow", False))
+	except (frappe.DoesNotExistError, AttributeError):
+		return False
